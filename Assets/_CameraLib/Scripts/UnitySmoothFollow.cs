e95986c6e80a1adefac32f96feca461daf9edc10
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2010-07-19 16:02:14 +0200

    Include lost meta files

---
+++ Assets/_CameraLib/Scripts/UnitySmoothFollow.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A implementation of Unitys smooth follow script (Standard assets) implemented to fit this library.
+/// 
+/// Original documentation:
+/// This camera smoothes out rotation around the y-axis and height.
+/// Horizontal Distance to the target is always fixed.
+/// 
+/// There are many different ways to smooth the rotation but doing it this way gives you a lot of control over how the camera behaves.
+/// 
+/// For every of those smoothed values we calculate the wanted value and the current value.
+/// Then we smooth it using the Lerp function.
+/// Then we apply the smoothed values to the transform's position.
+/// </summary>
+[AddComponentMenu("CameraLib/Smooth follow")]
+[RequireComponent (typeof (Camera))]
+public class UnitySmoothFollow : ICamera {
+	/// <summary>
+	/// The distance in the x-z plane to the target
+	/// </summary> 
+	public float distance = 10.0f;
+	/// <summary>
+	/// the height we want the camera to be above the target
+	/// </summary> 
+	public float height = 5.0f;
+	public float  heightDamping = 2.0f;
+	public float rotationDamping = 3.0f;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public override void UpdateCameraPosition() {
+		// Early out if we don't have a target
+		if (target == null){
+			return;
+		}
+		target.position = GetCameraTargetPosition();
+	}
+	
+	public override Vector3 GetCameraTargetPosition(){
+		Vector3 position = target.position;
+		// Calculate the current rotation angles
+		float wantedRotationAngle = target.eulerAngles.y;
+		float wantedHeight = target.position.y + height;
+			
+		float currentRotationAngle = transform.eulerAngles.y;
+		float currentHeight = transform.position.y;
+		
+		// Damp the rotation around the y-axis
+		currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+	
+		// Damp the height
+		currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+	
+		// Convert the angle into a rotation
+		Quaternion currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+		
+		// Set the position of the camera on the x-z plane to:
+		// distance meters behind the target
+		position -= currentRotation * (Vector3.forward * distance);
+	
+		// Set the height of the camera
+		position.y = currentHeight;
+		return position;
+	}
+}
