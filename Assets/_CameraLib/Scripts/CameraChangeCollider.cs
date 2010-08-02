14876a4e6094b9bae719b54e067f68ef15164856
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2010-08-02 09:37:22 +0200

    Improve composite camera

--- Assets/_CameraLib/Scripts/CameraChangeCollider.cs
+++ Assets/_CameraLib/Scripts/CameraChangeCollider.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 /// Change the camera when the target enters the collider (and change camera back, when target exists collider).
@@ -11,7 +12,7 @@ public class CameraChangeCollider : MonoBehaviour {
 	public Transform cameraTarget;
 	public CompositeCamera compositeCamera;
 	public int newCamera = 0;
-	private int oldCamera = 0;
+	private static List<int> cameraStack = new  List<int>();
 	public float interpolationTime = 1;
 	
 	void Start(){
@@ -22,14 +23,26 @@ public class CameraChangeCollider : MonoBehaviour {
 	
 	void OnTriggerEnter(Collider other) {
 		if (other.transform==cameraTarget){
-			oldCamera = compositeCamera.currentCamera;
+			int oldCamera = compositeCamera.currentCamera;
+			cameraStack.Add(oldCamera);
 			compositeCamera.SetCamera(newCamera, interpolationTime);
 		}
 	}
 	
 	void OnTriggerExit(Collider other) {
 		if (other.transform==cameraTarget){
-			compositeCamera.SetCamera(oldCamera, interpolationTime);
+			if (cameraStack[cameraStack.Count-1] == newCamera){
+				// if current camera, then pop
+				cameraStack.RemoveAt(cameraStack.Count-1);
+				compositeCamera.SetCamera(cameraStack[cameraStack.Count-1], interpolationTime);
+			} else {
+				int lastIndex = cameraStack.LastIndexOf(newCamera);
+				if (lastIndex>-1){
+					cameraStack.RemoveAt(lastIndex);	
+				} else {
+					Debug.LogWarning("Error in CameraChangeCollider. Cannot find camera "+newCamera+" in the camera stack");
+				}
+			}
 		}
 	}
 }
